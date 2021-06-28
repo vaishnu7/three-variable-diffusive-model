ad3caa99bd0b17d025e57d1580a82aad3a29a820
Vaishnudebi Dutta <39788520+vaishnu7@users.noreply.github.com>
2021-06-28 12:37:09 +0530

    Update code.m

--- code.m
+++ code.m
@@ -3,57 +3,57 @@ clear;
 close all;
 format long
 %parameters in the model
-a = 1.1;
-b = 0.6;
-d = 0.7;
-f = 1.2;
-K = 1;
-D1 = 1;
-D2 = 1;
-m = 0.1;
-c = 0.25;
+b = 6.0;
+a = 36.0;
+g = 45.0; %The positivity of g implies that there is a constant supply of activator and inhibitor ions
+sig = 9.0;
+d = 1.5;
 
-L=20;
-T=10;
-dx = 0.2;
-dt = 0.01;
+dx = 0.8;
+dy = 0.8;
+dt = 0.0025;
+L=10;
+T=1000;
 x=0:dx:L;
+y=0:dy:L;
 t=0:dt:T;
 Nx = length(x);
 Nt = length(t);
+Ny = length(y);
 
-e=dt/(dx.*dx);
-
+nu1=dt/(dx.*dx);
+nu2=dt/(dy.*dy);
 
 %Boundary Conditions
- U(1,2:Nt) = 1;
- W(1,2:Nt) = 1;
- U(Nx-1,2:Nt) = exp(1);
- W(Nx-1,2:Nt) = exp(1);
+ U(1:Nx,1,2:Nt) = (a/5) + (g*(b-4))/(5*b);
+ U(1:Nx,Ny,2:Nt) = (a/5) + (g*(b-4))/(5*b);
+ U(Nx,1:Ny,2:Nt)= (a/5) + (g*(b-4))/(5*b);
+ U(1,1:Ny,2:Nt)= (a/5) + (g*(b-4))/(5*b);
+ V(1:Nx,1,2:Nt) = (1 + g/ b.*U(1:Nx,1,2:Nt)).*(1 + U(1:Nx,1,2:Nt).^2);
+ V(1:Nx,Ny,2:Nt) = (1 + g/ b.*U(1:Nx,Ny,2:Nt)).*(1 + U(1:Nx,Ny,2:Nt).^2);
+ V(Nx,1:Ny,2:Nt)= (1 + g/ b.*U(Nx,1:Ny,2:Nt)).*(1 + U(Nx,1:Ny,2:Nt).^2);
+ V(1,1:Ny,2:Nt)= (1 + g/ b.*U(1,1:Ny,2:Nt)).*(1 + U(1,1:Ny,2:Nt).^2); 
  
- U(2,2:Nt) = U(1,2:Nt);
- W(2,2:Nt) = W(1,2:Nt);
- U(Nx,2:Nt) = U(Nx-1,2:Nt);
- W(Nx,2:Nt) = W(Nx-1,2:Nt);
- 
- %Initial conditions
-U(1:Nx,1) = 1.8.*(exp (-(x(1:Nx)-0.2).^2));
-W(1:Nx,1) = 0.3.*(exp (-(x(1:Nx)-0.7).^2));
 
-%---------------------------------------------------%
+%Initial conditions
+U(1:Nx,1:Ny,1) = 0;
+V(1:Nx,1:Ny,1) = 0;
 
 % Finite Difference Method
 
 for n = 1:Nt-1
-       
-    for  j=3:Nx-1
     
-    U(j,n+1)= e.* (U(j+1,n) - 2.*U(j,n) + U(j-1,n)) + U(j,n)+ U(j,n).*( a*dt - a*dt.*(U(j,n))./K - b*dt.*W(j,n)./(1 + m.*U(j,n))) ;
-    W(j,n+1)= e.* (W(j+1,n) - 2.*W(j,n) + W(j-1,n)) + W(j,n) + W(j,n).*( - d*dt - f.*dt.*U(j,n)./(1 + m.*U(j,n)));
-    
+    for  k=2:Nx-1
+        
+        for j=2:Ny-1
+            U(j,k,n+1) = U(j,k,n) + a*dt - dt.*U(j,k,n) - dt.*(4.*U(j,k,n).*V(j,k,n))./(1+U(j,k,n).^2) + dt*g + nu1.*(U(j+1,k,n)-2.*U(j,k,n)+U(j-1,k,n))+ nu2.*(U(j,k+1,n)-2.*U(j,k,n)+U(j,k-1,n)); 
+            V(j,k,n+1) = V(j,k,n) + sig.* ( dt*b.*(U(j,k,n)- (U(j,k,n).*V(j,k,n))./(1+U(j,k,n).^2)) + dt*g + d.*nu1.*(V(j+1,k,n)-2.*V(j,k,n)+V(j-1,k,n))+ d.*nu2.*(V(j,k+1,n)-2.*V(j,k,n)+V(j,k-1,n)));
+        end
+        
     end
+    
 end
-% plot(U(:,1))
-% hold on
-% plot (W(:,1))
-surf(U)
+
+surf(U) %prey graph
+%surf (V) %predator graph
+%contour (U) %if you want to plot contour map
